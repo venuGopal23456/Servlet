17a642af536ee82b5d459bb72cbe190e62f34481
venu vajeravelu <venuvajeravelu@VenuKingPin.local>
2021-03-21 13:49:21 -0700

    Basic Servlet Operations

---
+++ src/com/firstServlet/AddServlet.java
@@ -0,0 +1,55 @@
+package com.firstServlet;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@SuppressWarnings("serial")
+public class AddServlet extends HttpServlet{
+	
+	//No Need to create Object for HttpServletRequest and HttpServletResponse,No need to create Object,Instead Tomcat will do that we just need to create Reference.
+	//It is an Interface
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	{
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		
+		//Send Data from one Servlet to Another use Session Management or setAttribute()
+		//one k for Key and anothe for Value
+//		req.setAttribute("k", k);
+		
+		//Two way to Make a call to another Servlet
+		//1)Request Dispatcher 
+//		RequestDispatcher requestDispatcher = req.getRequestDispatcher("sq");
+//		requestDispatcher.forward(req, res);
+		
+		//2)Redirecting(Session Management - 2 concepts)
+//		  	2a)URL Rewriting//it Fails for multiple Data
+		//Telling the Browser to look for this Servlet from AddServlet
+//		res.sendRedirect("sq?k=" + k);
+		
+//			2b)Session(Given by Tomcat,we need to just get the hold of it)
+//		HttpSession session = req.getSession();
+//		session.setAttribute("k", k);
+//		res.sendRedirect("sq");
+		
+//			2c)Cookies
+		//we cant pass cookie a int k it should be String for that append "" 
+		Cookie cookie = new Cookie("k", k + "");
+		res.addCookie(cookie);
+		res.sendRedirect("sq");
+		
+		
+		
+	}
+
+}
---
+++ src/com/firstServlet/SquareServlet.java
@@ -0,0 +1,53 @@
+package com.firstServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
+@SuppressWarnings("serial")
+public class SquareServlet extends HttpServlet   {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		//For ReqestDispatcher
+//		int k  = (int) req.getAttribute("k");
+		
+		//For Redirecting to get the Obj
+		//If we pass the ?k=9 in the sq url this will work if we dont pass in the previous Servlet
+//		int k = Integer.parseInt(req.getParameter("k"));
+		
+		//Using Seesion to get the Value
+		
+		HttpSession session = req.getSession();
+		//when we dont need value we can remove this k which will throw NPE
+		  //session.removeAttribute("k");
+//		int k = (int) session.getAttribute("k");
+//		
+//		k = k * k;
+//		PrintWriter outPrintWriter = res.getWriter();
+//		outPrintWriter.println("The is My page " + k);
+		
+		//Using cookie to get the Value
+		int k = 0;
+		Cookie cookies[] = req.getCookies();
+		
+		for(Cookie c : cookies)
+		{
+			if(c.getName().equals("k"))
+			{
+				k = Integer.parseInt(c.getValue());
+			}
+		}
+		k = k * k;
+		PrintWriter outPrintWriter = res.getWriter();
+		outPrintWriter.println("The is My page " + k);
+		
+	}
+
+}
